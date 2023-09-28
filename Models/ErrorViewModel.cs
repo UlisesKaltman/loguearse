9f55c7707b6147365a830b3b48147b52abf85ea8
ULISES MARTÍN KALTMAN <47870708@ort>
2023-09-28 14:11:22 -0300

    calc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace loguearse.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿function mostrarresultado(texto)
+{
+     document.getElementById("resultado").value=texto;
+}
+
+function mostraralert(texto)
+{
+    alert(texto);
+}
+
+function  sumar()
+{
+    mostrarresultado(parseInt(document.getElementById("nro1").value) + parseInt(document.getElementById("nro2").value));
+}
+
+function  restar()
+{
+    mostrarresultado(parseInt(document.getElementById("nro1").value) - parseInt(document.getElementById("nro2").value));
+}
+
+function  multiplicar()
+{
+    mostrarresultado(parseInt(document.getElementById("nro1").value) * parseInt(document.getElementById("nro2").value));
+}
+
+function  dividir()
+{
+    mostrarresultado(parseInt(document.getElementById("nro1").value) / parseInt(document.getElementById("nro2").value));
+}
